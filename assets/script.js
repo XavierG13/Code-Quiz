969171a3d7a73234dacd54f2cf3c6c82b6befe58
Xavier Guzman <guzmanxavi112@gmail.com>
2020-11-17 16:53:35 -0600

    added event listener for timer

--- assets/script.js
+++ assets/script.js
@@ -5,7 +5,80 @@ var questionHolderEl = document.getElementById("#questions");
 var highScoresEl = document.getElementById("#highScoresHolder");
 var goBackBtn = document.getElementById("#goBackBtn");
 var clearBtnEl = document.getElementById("#clearBtn");
+// variables used for the timer
+var timeEl = document.getElementById("#timerHolder");
+
+var time = 75;
+var score = 0;
+var subtractTime = 15;
+var timer = 0;
+
 // var questions arrary for questions
-var questions = []
+var questionCount = 0;
+var questions = [
+    {
+        title: "What does HTML stand for?",
+        choices: ["Hypertext Makeup Language", "How Text Makes Language", "Here This Makes Language", "Hyper Text Make Language"],
+        answer: "Hypertext Makeup Language"
+    },
+
+    {
+        title: "",
+        choices: "",
+        answer: ""
+    },
+
+    {
+        title: "",
+        choices: "",
+        answer: ""
+    },
+
+    {
+        title: "",
+        choices: "",
+        answer: ""
+    },
+
+    {
+        title: "",
+        choices: "",
+        answer: ""
+    },
+
+]
+
+// Listeners for buttons
+timeEl.addEventListener.on("click", function () {
+    if (timer === 0) {
+        timer.setInterval(function () {
+            secondsLeft--;
+            timerHolder.creatext = document.getElementById("timer-holder") + secondsLeft;
+            if (secondsLeft <= 0) {
+                clearInterval(timer);
+                allDone();
+                currentTime.textContent = "Quiz is over.";
+            }
+        }, 1000);
+    }
+    render(questions);
+});
+// Functions for questions and answers
+
+questions.forEach(function (question) {
+
+    for (i = 0; i < questions.length; i++) {
+        var question = questions.array[i];
+
+        var li = document.createElement('li');
+        var questionsTextEl = document.createTextNode(question);
+
+        li.appendChild(questionsTextEl);
+        document.getElementById("#questionsUl").appendChild(li);
+
+        console.log(question);
+    }
+})
+// this function will loop through the questions array and move them to the html
 
 
